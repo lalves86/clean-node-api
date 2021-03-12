c22afe99bdf16b5a52743c3b1c1b9fb962b88cf2
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-03-12 08:17:13 -0300

    refactor: move data layer interfaces to folders

--- src/infra/log-repository/log.ts
+++ src/infra/log-repository/log.ts
@@ -1,4 +1,4 @@
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { MongoHelper } from '../db/mongodb/helpers/mongo-helper'
 
 export class LogMongoRepository implements LogErrorRepository {
