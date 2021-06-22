a7bb1bd7841c0cb6b2fa7616d02100726db49a8d
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-06-22 08:19:23 -0300

    refactor: organize project files

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -2,7 +2,7 @@ import { LoginController } from './login-controller'
 import { Validation, HttpRequest, Authentication } from './login-controller-protocols'
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest, ok, serverError, unauthorized } from '@/presentation/helper/http/http-helper'
-import { AuthenticationModel } from '@/domain/usecases/authentication'
+import { AuthenticationModel } from '@/domain/usecases/account/authentication'
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
