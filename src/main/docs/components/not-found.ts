cd810431ac8162b1f6502a474d44332352dc93dd
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-06-30 08:39:22 -0300

    docs: update login api docs

---
+++ src/main/docs/components/not-found.ts
@@ -0,0 +1,3 @@
+export const notFound = {
+  description: 'Página não encontrada'
+}
--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,36 @@
+import { loginPath } from './paths'
+import { badRequest, serverError, unauthorized, notFound } from './components'
+import { accountSchema, errorSchema, loginParamsSchema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    unauthorized,
+    notFound,
+    serverError
   }
 }
