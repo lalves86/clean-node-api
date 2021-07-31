3d489780a896b8d25475f882eb75b3ba40a86cfe
lalves86 <lucas.alves@engenharia.ufjf.br>
2021-07-31 09:03:10 -0300

    test: ensure surveyResult query returns SurveyResult on success

--- src/main/graphql/resolvers/survey-result.ts
+++ src/main/graphql/resolvers/survey-result.ts
@@ -4,10 +4,10 @@ import { makeSaveSurveyResultController } from '@/main/factories/controllers/sru
 
 export default {
   Query: {
-    surveyResult: async (parent: any, args: any) => await adaptResolver(makeLoadSurveyResultController())
+    surveyResult: async (parent: any, args: any, context: any) => await adaptResolver(makeLoadSurveyResultController(), args, context)
   },
 
   Mutation: {
-    saveSurveyResult: async (parent: any, args: any) => await adaptResolver(makeSaveSurveyResultController())
+    saveSurveyResult: async (parent: any, args: any, context: any) => await adaptResolver(makeSaveSurveyResultController(), args, context)
   }
 }
